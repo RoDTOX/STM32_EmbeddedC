208a0eec50463c858add20410ced8586a3303a7d
Bogdan Seceleanu <bogdan.seceleanu@partner.magna.com>
2024-09-17 11:42:55 +0300

    Save

--- 1_EmbeddedC/Embedded-C-master/Bogdan/target/167_BitfieldExercise/Inc/main.h
+++ 1_EmbeddedC/Embedded-C-master/Bogdan/target/167_BitfieldExercise/Inc/main.h
@@ -4,6 +4,7 @@
  *  Created on: Sep 16, 2024
  *      Author: bogdsece
  */
+#include <stdint.h>
 
 #ifndef MAIN_H_
 #define MAIN_H_
@@ -40,5 +41,53 @@ typedef struct
 	uint32_t RESERVED_4:1;
 
 }RCC_AHB1ENR_t;
+
+typedef struct
+{
+	/*
+	These bits are written by software to configure the I/O direction mode.
+	00: Input (reset state)
+	01: General purpose output mode
+	10: Alternate function mode
+	11: Analog mode
+	*/
+	uint32_t PIN_0:2;
+	uint32_t PIN_1:2;
+	uint32_t PIN_2:2;
+	uint32_t PIN_3:2;
+	uint32_t PIN_4:2;
+	uint32_t PIN_5:2;
+	uint32_t PIN_6:2;
+	uint32_t PIN_7:2;
+	uint32_t PIN_8:2;
+	uint32_t PIN_9:2;
+	uint32_t PIN_10:2;
+	uint32_t PIN_11:2;
+	uint32_t PIN_12:2;
+	uint32_t PIN_13:2;
+	uint32_t PIN_14:2;
+	uint32_t PIN_15:2;
+}GPIOx_MODE_t;
+
+typedef struct
+{
+	uint32_t PIN_0:1;
+	uint32_t PIN_1:1;
+	uint32_t PIN_2:1;
+	uint32_t PIN_3:1;
+	uint32_t PIN_4:1;
+	uint32_t PIN_5:1;
+	uint32_t PIN_6:1;
+	uint32_t PIN_7:1;
+	uint32_t PIN_8:1;
+	uint32_t PIN_9:1;
+	uint32_t PIN_10:1;
+	uint32_t PIN_11:1;
+	uint32_t PIN_12:1;
+	uint32_t PIN_13:1;
+	uint32_t PIN_14:1;
+	uint32_t PIN_15:1;
+	uint32_t RESERVED:16;
+}GPIOx_PIN_t;
 
 #endif /* MAIN_H_ */
--- 1_EmbeddedC/Embedded-C-master/Bogdan/target/167_BitfieldExercise/Src/main.c
+++ 1_EmbeddedC/Embedded-C-master/Bogdan/target/167_BitfieldExercise/Src/main.c
@@ -24,16 +24,46 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+extern void initialise_monitor_handles(void);
+
 int main(void)
 {
+	initialise_monitor_handles();
     /* Loop forever */
-	printf("Hello world!\n");
-	printf("sizeof struct RCC_AHB1ENR_t = %d",sizeof(RCC_AHB1ENR_t));
+	printf("sizeof struct RCC_AHB1ENR_t = %d\n",sizeof(RCC_AHB1ENR_t));
 	printf("Size of short = %d\n", sizeof(short));
 	printf("Size of int = %d\n", sizeof(int));
 	printf("Size of long = %d\n", sizeof(long));
 	printf("Size of long long = %d\n", sizeof(long long));
 	printf("Size of double = %d\n", sizeof(double));
+
+	//0. Set registers according to Cortex M4 datasheet
+	RCC_AHB1ENR_t volatile * const pClockControl = 	(RCC_AHB1ENR_t *)0x40023830;
+	GPIOx_MODE_t volatile * const pPortDMode = 		(GPIOx_MODE_t *)0x40020C00;
+	GPIOx_PIN_t volatile * const pPortDOut = 		(GPIOx_PIN_t *)0x40020C14;
+	//1. Enable the clock for GPIOD pheripheral in the ahb1enr + SET 3rd bit position
+	pClockControl->GPIO_DEN = 1;
+
+	//2. Configure the mode of the IO pin as output:
+	//a. clear the 24th and 25th bit positions (CLEAR)
+	//b. make 24th bit position as 1 (SET)
+	pPortDMode->PIN_12 = 0b01;
+
+	while(1)
+	{
+		//3.SET 12th bit of the output data register to make I/O pin-12 as HIGH
+		pPortDOut->PIN_12 = 1;
+
+		//introduce small human observable delay
+		//This loop executes for 10K times
+		for(uint32_t i=0 ; i < 300000 ; i++ );
+
+		//Tun OFF the LED
+		pPortDOut->PIN_12 = 0;
+
+		for(uint32_t i=0 ; i < 300000 ; i++ );
+	}
+
 
 	return 0;
 	for(;;);
