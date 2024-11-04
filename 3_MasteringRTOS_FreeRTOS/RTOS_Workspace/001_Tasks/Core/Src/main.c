af0a782b713e16c7d1c7fb35efe2fc7cb0fd8378
Bogdan-PCPers <bogdan.seceleanu@yahoo.com>
2024-11-04 14:50:09 +0200

    SEGGER SysView continous recording implementation - not working atm

--- 3_MasteringRTOS_FreeRTOS/RTOS_Workspace/001_Tasks/Core/Src/main.c
+++ 3_MasteringRTOS_FreeRTOS/RTOS_Workspace/001_Tasks/Core/Src/main.c
@@ -71,6 +71,8 @@ static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 static void task1_handler(void* parameters);
 static void task2_handler(void* parameters);
+
+void SEGGER_UART_init(U32 baud);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -114,8 +116,11 @@ int main(void)
     //Enable the cycle counting for Segger SysView tool
   	DWT_CTRL |= 1;
 
+  	SEGGER_UART_init(500000);
+
   	SEGGER_SYSVIEW_Conf();
-  	SEGGER_SYSVIEW_Start();
+
+  	//SEGGER_SYSVIEW_Start(); 	//Commented because this command is done in the segger_uart_init()
 
 	Ret = xTaskCreate(task1_handler, "task1", 200, "HelloWorldFromTask1", 2, &task1_handle);
 
@@ -166,8 +171,8 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 50;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -183,7 +188,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
